d0fd2ec3ad65449d2c984fe8713c3dc4f3c7a5d1
nitz <nitz.raz@gmail.com>
2017-05-16 19:08:53 +0300

    more sentence, nicer print

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var Main = require('./buildAlg/main');
  
 var sentences = [
   "sugar,pepper, water, butter and without lemon juice and does not contain milk",
+  "i want cake with sugar,pepper, water, butter and without lemon juice and does not contain milk",
 ]
 
 for (var i = 0, len = sentences.length; i < len; i++) {
   var sentence = sentences[i];
   var res= Main.sentence_to_sql(sentence);
-  console.log("---\n%s:\n%j", sentence, res);
+  console.log("===\n%s:\n---\n%j", sentence, res);
 }
